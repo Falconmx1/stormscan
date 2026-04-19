0e7f13b5328f7f74bd1b6d4bd8d1822738ee2a5c
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-04-18 19:01:00 -0600

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,4 @@
+# PowerShell como Administrador
+Write-Host "⚡ Instalando StormScan..." -ForegroundColor Cyan
+pip install -r requirements.txt
+Write-Host "✅ Instalación completa. Usa 'python main.py <target>'" -ForegroundColor Green
